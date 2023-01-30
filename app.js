0cc608195282d8f3b408608f53573ff13c4839d0
Luiz Almeida <luizengdev@gmail.com>
2023-01-30 19:49:03 -0300

    ajustes no css

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function getCurrentLocationWeather(lat, lon) {
 }
 
 function getCityWeather(cityName) {
-  weatherIcon.src = "/assets/loading-icon.svg";
+  weatherIcon.src = "./assets/loading-icon.svg";
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${api_key}`
   )
@@ -70,10 +70,10 @@ function displayWeather(data) {
 
   currentDate.textContent = formatDate(dt);
   cityName.textContent = name;
-  weatherIcon.src = `/assets/${icon}.svg`;
+  weatherIcon.src = `./assets/${icon}.svg`;
   weatherDescription.textContent = description;
   currentTemperature.textContent = `${Math.round(temp)}°C`;
-  windSpeed.textContent = `${Math.round(speed * 3.6)}km`;
+  windSpeed.textContent = `${Math.round(speed * 3.6)}km/h`;
   feelsLikeTemperature.textContent = `${Math.round(feels_like)}°C`;
   currentHumidity.textContent = `${humidity}%`;
   suriseTime.textContent = formateTime(sunrise);
